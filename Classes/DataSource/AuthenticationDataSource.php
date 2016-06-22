8386cefe657de9275c7cb73201aa0b80406435c8
Bastian <b.waidelich@wwwision.de>
2016-06-22 10:18:34 +0200

    * Add more authentication details
    * Authenticated account
    * Authenticated roles

--- Classes/DataSource/AuthenticationDataSource.php
+++ Classes/DataSource/AuthenticationDataSource.php
@@ -4,6 +4,7 @@ namespace Wwwision\Clockwork\DataSource;
 use Clockwork\DataSource\DataSourceInterface;
 use Clockwork\Request\Request as ClockworkRequest;
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Persistence\PersistenceManagerInterface;
 use TYPO3\Flow\Security\Context;
 
 /**
@@ -19,18 +20,36 @@ class AuthenticationDataSource implements DataSourceInterface
     protected $securityContext;
 
     /**
+     * @Flow\Inject
+     * @var PersistenceManagerInterface
+     */
+    protected $persistenceManager;
+
+    /**
      * @param ClockworkRequest $clockworkRequest
      * @return ClockworkRequest
      */
     public function resolve(ClockworkRequest $clockworkRequest)
     {
+        $authenticationTokens = [];
         foreach ($this->securityContext->getAuthenticationTokens() as $token) {
             if ($token->isAuthenticated()) {
-                $clockworkRequest->sessionData['Authenticated tokens'][$token->getAuthenticationProviderName()][] = get_class($token);
+                $authenticationTokens[$token->getAuthenticationProviderName()][get_class($token)] = 'AUTHENTICATED';
             } else {
-                $clockworkRequest->sessionData['Remaining tokens'][$token->getAuthenticationProviderName()][] = get_class($token);
+                $authenticationTokens[$token->getAuthenticationProviderName()][get_class($token)] = 'not authenticated';
             }
         }
+        $authenticatedAccount = $this->securityContext->getAccount();
+        if ($authenticatedAccount !== null) {
+            $authenticatedAccountIdentifier = $authenticatedAccount->getAccountIdentifier() . ' (' . $this->persistenceManager->getIdentifierByObject($authenticatedAccount) . ')';
+        } else {
+            $authenticatedAccountIdentifier = '-';
+        }
+        $clockworkRequest->sessionData['Flow Authentication'] = [
+            'Authentication tokens' => $authenticationTokens,
+            'Account' => $authenticatedAccountIdentifier,
+            'Roles' => array_keys($this->securityContext->getRoles()),
+        ];
         return $clockworkRequest;
     }
 }
